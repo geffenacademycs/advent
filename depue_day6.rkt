d654727087d4aaa9023d9cd13fdf6c6614876b4a
wdepue <30643741+wdepue@users.noreply.github.com>
2019-12-11 13:16:08 -0800

    Update depue_day6.rkt

--- depue_day6.rkt
+++ depue_day6.rkt
@@ -3,23 +3,23 @@
 #reader(lib "htdp-intermediate-lambda-reader.ss" "lang")((modname depue_day6) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "while.rkt" "installed-teachpacks") (lib "until.rkt" "installed-teachpacks") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "while.rkt" "installed-teachpacks") (lib "until.rkt" "installed-teachpacks") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 (require 2htdp/batch-io)
 
-(define (direct lst) (length lst))
+(define (direct lst) (length lst)) ; since the input contains all branches, the length of the list of branches is the number of direct orbits
 
-(define (interpret lst output) (cond
-    [(empty? lst) output]
+(define (interpret lst output) (cond ; this is to interpret codes in the format "GBH)EKR" to (list (list num num num) (list num num num))
+    [(empty? lst) output] ; i converted strings to list of characters and individual characters to numbers
     [else (interpret (rest lst) (cons (organize (intcodes (first lst))) output))]))
 
-(define (intcodes strng) ; changes strings into a list of 3 items.
-  (map char->integer
+(define (intcodes strng) ; changes strings into a list of characters and then characters to integers.
+  (map char->integer ; for example, "GBH)EKR" -> (list num num num num num num num)
           (string->list strng)))
 
-(define (organize lst)
-  (list
-   (list (first lst) (second lst) (third lst))
-   (list (fifth lst) (sixth lst) (seventh lst))))
+(define (organize lst) ; this is the organize function 
+  (list ; while this may seem vestigial, this helps a lot when it comes to organize-reverse and backtracking for me
+   (list (first lst) (second lst) (third lst)) ; a better and rewritten solution would be less shitty but whatever
+   (list (fifth lst) (sixth lst) (seventh lst)))) ; this still works
 
 (define (organize-reverse lst)
-  (list
+  (list ; reverse of organize, where the second object comes first
    (list (fifth lst) (sixth lst) (seventh lst))
    (list (first lst) (second lst) (third lst))))
 
@@ -57,16 +57,24 @@
 
 (define (lowestcommonpath lst1b lst1 lst2 currentlowest lst) (cond
       [(empty? lst1) lst1b]
-      [(matches? (first (first (first lst1))) (first (first (first lst2)))) (first (tracepath currentlowest lst 0 empty))]
-      [else (lowestcommonpath lst1b (rest lst1) (rest lst2) (if (empty? (first (first (first lst1)))) currentlowest (first (first (first lst1)))) lst)]
-                                                     ))
+      [(matches? (first (first (first lst1))) 
+                 (first (first (first lst2)))) 
+                        (first (tracepath currentlowest lst 0 empty))]
+      [else (lowestcommonpath lst1b (rest lst1) (rest lst2) 
+                              (if (empty? (first (first (first lst1)))) 
+                                          currentlowest 
+                                  (first (first (first lst1)))) 
+                              lst)]))
 
 (define (matches? lst1 lst2) (if
-    (and (eq? (first lst1) (first lst2)) (eq? (second lst1) (second lst2)) (eq? (third lst1) (third lst2))) false true
+    (and (eq? (first lst1) (first lst2)) 
+         (eq? (second lst1) (second lst2)) 
+         (eq? (third lst1) (third lst2))) 
+         false 
+         true
   ))
 
-(define (tree-distance name1 name2 file) (tree-distancex (tracepath (intcodes name1)
+(define (tree-distance name1 name2 file) ; starter function for tree-distancex
+(tree-distancex (tracepath (intcodes name1)
                      (interpret-reverse (read-lines file) empty) 0 empty) (tracepath (intcodes name2)
                      (interpret-reverse (read-lines file) empty) 0 empty) (interpret-reverse (read-lines file) empty)))
-
-(tree-distance "YOU" "SAN" "depue_day6.txt")
